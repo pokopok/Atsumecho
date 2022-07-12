3d8b03b0fbfb12673a9d25de67775b91375f72ab
pokopok <301lv.hjtm-pq@hitokura.net>
2022-07-13 07:29:00 +0900

    add add_goshuin

--- goshuin_books/urls.py
+++ goshuin_books/urls.py
@@ -6,5 +6,6 @@ app_name = 'goshuin_books'
 urlpatterns = [
     path('create_book', views.GoshuinBookCreateView.as_view(), name='create_book'),
     path('list_book', views.GoshuinBookListView.as_view(), name='list_book'),
-    path('book/<int:book>', views.GoshuinListView.as_view(), name='book'),
+    path('book/<int:book_id>', views.GoshuinListView.as_view(), name='book'),
+    path('add_goshuin/<int:book_id>', views.GoshuinAddView.as_view(), name='add_goshuin'),
 ]
--- goshuin_books/views.py
+++ goshuin_books/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import get_object_or_404, render
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from .models import GoshuinBooks, Goshuins
 import os
+from datetime import date
 
 class GoshuinBookCreateView(CreateView):
     model = GoshuinBooks
@@ -18,7 +19,7 @@ class GoshuinBookCreateView(CreateView):
 
     def get_form(self):
         form = super(GoshuinBookCreateView, self).get_form()
-        form.fields['name'].label = '御朱印帳の名前'
+        form.fields['name'].label = 'ご朱印帳の名前'
         return form
 
 class GoshuinBookListView(ListView):
@@ -36,11 +37,32 @@ class GoshuinListView(ListView):
 
     def get_queryset(self):
         qs = super(GoshuinListView, self).get_queryset()
-        qs = qs.filter(goshuin_book=self.kwargs['book'])
+        qs = qs.filter(goshuin_book=self.kwargs['book_id'])
         return qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        book = get_object_or_404(GoshuinBooks, id=self.kwargs['book'])
+        book = get_object_or_404(GoshuinBooks, id=self.kwargs['book_id'])
         context['book_name'] = book.name 
         return context
+
+class GoshuinAddView(CreateView):
+    model = Goshuins
+    fields = ['name', 'date', 'picture', 'memo']
+    template_name = os.path.join('goshuin_books', 'add_goshuin.html')
+    # success_url = reverse_lazy('goshuin_books:list_book')
+
+    def get_success_url(self):
+        return reverse('goshuin_books:book', kwargs={'book_id': self.object.goshuin_book.id})
+
+    def form_valid(self, form): #フォーム送信前に実行される
+        form.instance.goshuin_book = get_object_or_404(GoshuinBooks, id=self.kwargs['book_id'])
+        return super(GoshuinAddView, self).form_valid(form)
+
+    def get_form(self):
+        form = super(GoshuinAddView, self).get_form()
+        form.fields['name'].label = 'ご朱印の名前'
+        form.fields['date'].label = '日付'
+        form.fields['picture'].label = '写真'
+        form.fields['date'].initial = date.today()
+        return form
