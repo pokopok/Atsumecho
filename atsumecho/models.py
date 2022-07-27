33781d2629d4e2aa85a810b000499f520b34af2e
pokopok <301lv.hjtm-pq@hitokura.net>
2022-07-27 15:49:50 +0900

    goshuin_bookからatsumechoへ変更

--- atsumecho/models.py
+++ atsumecho/models.py
@@ -5,36 +5,36 @@ from accounts.models import Users
 from django.dispatch import receiver
 import os
 
-class GoshuinBooks(models.Model):
+class Books(models.Model):
     name = models.CharField(max_length=30)
     user = models.ForeignKey(
         Users, on_delete=models.CASCADE
     )
 
     class Meta:
-        db_table = 'goshuin_books'
+        db_table = 'books'
 
     def __str__(self):
         return self.name
 
-class Goshuins(models.Model):
+class Records(models.Model):
     name = models.CharField(max_length=30)
     date = models.DateField(default=date.today())
-    picture = models.FileField(upload_to='goshuin_pictures')
+    picture = models.FileField(upload_to='record_pictures')
     memo = models.CharField(max_length=255, blank=True)
-    goshuin_book = models.ForeignKey(
-        GoshuinBooks, on_delete=models.CASCADE
+    book = models.ForeignKey(
+        Books, on_delete=models.CASCADE
     )
 
     class Meta:
-        db_table = 'goshuins'
+        db_table = 'records'
     
     def __str__(self):
         return self.name
 
 
-@receiver(models.signals.post_delete, sender=Goshuins)
-#Goshuinsが削除された場合に画像も削除
+@receiver(models.signals.post_delete, sender=Records)
+#recordが削除された場合に画像も削除
 def delete_picture(sender, instance, **kwargs):
     if instance.picture:
         if os.path.isfile(instance.picture.path):
--- project/urls.py
+++ project/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('goshuin_books/', include('goshuin_books.urls')),
+    path('atsumecho/', include('atsumecho.urls')),
 ]
 
 if settings.DEBUG:
